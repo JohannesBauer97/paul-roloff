cdebd6e3960ad478ee417bd3f8d708251ff60a33
Joba <me@johannes-bauer.de>
2023-04-14 16:20:58 +0200

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private renderer: Renderer2) {
+    }
+
+    ngOnInit(): void {
+        const randomNumber = Math.floor(Math.random() * 20);
+        const backgroundImagePath = `/assets/backgrounds/background-${randomNumber}.png`;
+        this.renderer.setStyle(document.body, 'background-image', `url("${backgroundImagePath}")`);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
