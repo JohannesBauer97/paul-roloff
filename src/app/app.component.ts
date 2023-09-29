397c9a6796bdc58e3c9a80142ada1de721fe00c6
Joba <me@johannes-bauer.de>
2023-09-29 12:00:44 +0200

    Performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    constructor(private renderer: Renderer2) {
-    }
+  constructor(private renderer: Renderer2) {
+  }
 
-    ngOnInit(): void {
-        const randomNumber = Math.floor(Math.random() * 20);
-        const backgroundImagePath = `/assets/backgrounds/background-${randomNumber}.png`;
-        this.renderer.setStyle(document.body, 'background-image', `url("${backgroundImagePath}")`);
-    }
+  ngOnInit(): void {
+    const randomNumber = Math.floor(Math.random() * 20);
+    const backgroundImagePath = `/assets/backgrounds/background-${randomNumber}.webp`;
+    this.renderer.setStyle(document.body, 'background-image', `url("${backgroundImagePath}")`);
+  }
 }
